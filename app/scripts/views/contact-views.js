e46fac1ae78e7bbc3c6753e6c0c143f1d5425ca8
Luis Rene Onate <lonate12@gmail.com>
2016-10-19 21:52:14 -0400

    Added Views for ul and li.

--- app/scripts/views/contact-views.js
+++ app/scripts/views/contact-views.js
@@ -3,3 +3,21 @@ require('bootstrap-sass/assets/javascripts/bootstrap');
 var Backbone = require('backbone');
 var contactListTemplate = require('../../templates/contact-list-template.hbs');
 var contactHomeTemplate = require('../../templates/contact-list-template.hbs');
+
+
+var ContactListView = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'contact-list col-md-6 list-group',
+  initialize: function(){
+    this.listenTo(this.collection, 'add', this.renderContactItem);
+  },
+  renderContactItem: function(contact){
+    var contactItem = new ContactItemView({model: contact});
+    this.$el.append(contactItem.render().el);
+  }
+});
+
+var ContactItemView = Backbone.View.extend({
+  tagName: 'li',
+  className: 'list-group-item'
+});
